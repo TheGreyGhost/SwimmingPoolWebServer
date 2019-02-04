9ce46f6204dff17ff4e490d2135e57f27759b0a3
The Grey Ghost <5291905+TheGreyGhost@users.noreply.github.com>
2019-02-05 00:16:54 +1030

    added timestamp

--- php/get_current_sensor_readings.php
+++ php/get_current_sensor_readings.php
@@ -8,6 +8,17 @@
 
     echo "current water temperature (only valid if pump running):".round($row["hx_cold_inlet_smooth"],1)." C<br>";
     echo "current air temperature:".round($row["temp_ambient_smooth"],1)." C<br>";
+    $unix_timestamp = $row["timestamp"];
+    $datetime = new DateTime("@$unix_timestamp");
+
+	// convert from UTC (GMT) to local time
+    $date_time_format = $datetime->format('Y-m-d H:i:s');
+    $time_zone_from="UTC";
+    $time_zone_to='Australia/Adelaide';
+    $display_date = new DateTime($date_time_format, new DateTimeZone($time_zone_from));
+    $display_date->setTimezone(new DateTimeZone($time_zone_to));
+    echo "measured at ".$display_date->format('d-m-Y H:i:s')."<br>";   
+    
     mysqli_free_result($result);
     mysqli_close($conn_rt);
 ?>
