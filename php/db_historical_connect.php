de68c4101b6804d980fe18decdefe1acf9d70387
The Grey Ghost <5291905+TheGreyGhost@users.noreply.github.com>
2019-05-10 18:37:56 +0930

    test for graphing

--- php/db_historical_connect.php
+++ php/db_historical_connect.php
@@ -2,17 +2,17 @@
 // DB_REALTIME_CONNECT.PHP
     // Set database login details
     $servername = "localhost";
-    $username_rt = "webserverreadonly";
-    $password_rt = "gorflune";
-    $dbname_rt = "AutomationServerHistory";
+    $username_hist = "webserverreadonly";
+    $password_hist = "gorflune";
+    $dbname_hist = "AutomationServerHistory";
 
     // Create connection
 
-    $conn_rt = mysqli_connect($servername, $username_rt, $password_rt, $dbname_rt);
+    $conn_hist = mysqli_connect($servername, $username_hist, $password_hist, $dbname_hist);
 
     // Check connection
 
-    if (!$conn_rt) {
+    if (!$conn_hist) {
         die("Connection failed: " . mysqli_connect_error());
     }
 ?>
--- php/get_historical.php
+++ php/get_historical.php
@@ -1,44 +1,58 @@
 <?php
     require "db_historical_connect.php";
 
+    // Get the latest sensor reading, round the result for each sensor and update webpage
+    $sql = "SELECT `timestampUTC`, `hx_hot_inlet_ave`, `hx_hot_outlet_ave`, `hx_cold_inlet_ave`,  `hx_cold_outlet_ave`,  `temp_ambient_ave`,  `cumulative_insolation` FROM `LoggedData` ORDER BY timestampUTC LIMIT 5;";
+    $result = mysqli_query($conn_hist, $sql);
+    $historydata = mysqli_fetch_all($result);
+    mysqli_free_result($result);
+    mysqli_close($conn_hist);
 
-// not-shown: file bootstrapping logic, if any
-$rows = [];
-// not shown: your DB logic to retrieve records
-
-if(empty($rows)) {
+if(empty($historydata)) {
     // you have no results to work with so perhaps handle this as error condition and exit/redirect.
+  die("No data found");
 }
 ?>
+
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 <script type="text/javascript">
     // use json_encode to pass data from PHP to javascript literal
-    var rows = <?php echo json_encode($rows); ?>;
+    var historydata = <?php echo json_encode($historydata); ?>;
+
+   hdlen = historydata.length;	
+   for (i = 0; i < 3; i++) {
+     document.writeln(historydata[i][0]);
+   }
+
+   document.writeln("----");
+   for (i = 0; i < 3; i++) {
+     document.writeln(historydata[0][i]);
+   }
 
     //Google Stuff
-    google.charts.load('current', {packages: ['corechart']});
-    google.charts.setOnLoadCallback(drawChart);
+//    google.charts.load('current', {packages: ['corechart']});
+//    google.charts.setOnLoadCallback(drawChart);
 
-    function drawChart() {
+//    function drawChart() {
         // Define the chart to be drawn.
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Element');
-        data.addColumn('number', 'Percentage');
-        data.addRows(rows);         
+//        var data = new google.visualization.DataTable();
+//        data.addColumn('string', 'Element');
+//        data.addColumn('number', 'Percentage');
+//        data.addRows(rows);         
 
         // Set chart options
-        var options = {
-            title:'Sales',
-            titleTextStyle: {color: 'white', bold: true},
-            height:300,
-            backgroundColor: '#3a3939',
-            color: '#fff',
-            legend: {textStyle: {color: 'white'}},
-            hAxis: {gridlines: {color: 'black'}}
-        };
+  //      var options = {
+//            title:'Sales',
+//            titleTextStyle: {color: 'white', bold: true},
+//            height:300,
+//            backgroundColor: '#3a3939',
+//            color: '#fff',
+//            legend: {textStyle: {color: 'white'}},
+//            hAxis: {gridlines: {color: 'black'}}
+//        };
 
-        // Instantiate and draw the chart.
-        var chart = new google.visualization.PieChart(document.getElementById('SalesChar'));
-        chart.draw(data, options );
-    }
+//        // Instantiate and draw the chart.
+//        var chart = new google.visualization.PieChart(document.getElementById('SalesChar'));
+//        chart.draw(data, options );
+//    }
 </script>
