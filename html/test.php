a1075d0a9f96014ebbec4a6e31df753a373d06a4
The Grey Ghost <5291905+TheGreyGhost@users.noreply.github.com>
2019-05-11 14:22:01 +0930

    Prototype

--- html/test.php
+++ html/test.php
@@ -1,6 +1,54 @@
+<html>
+<head>
+<script type="text/javascript" src="./resources/extl/dygraph.js"></script>
+<link rel="stylesheet" href="./resources/extl/dygraph.css" />
+</head>
+<body>
+<div id="graphdiv"></div>
 <?php 
-
-echo "Pool temperature monitor:<br>";
 require __DIR__.'/../php/get_historical.php';
-echo "-----------------------<br>";
 ?>
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script type="text/javascript">
+  // use json_encode to pass data from PHP to javascript literal
+  var historydata = <?php echo json_encode($historydata); ?>;
+
+  var SECONDS_FROM_1970_TO_2000 = 946684800;
+  var unixtime = 0;	
+  document.write("start<br>");
+  // convert timestamp unixtime to javascript dates
+  hdrows = historydata.length;
+  hdcols = historydata[0].length	
+  for (i = 0; i < hdrows; i++) {
+    for (j=0; j < hdcols; ++j) 
+      historydata[i][j] = +historydata[i][j]; // convert string to number
+    unixtime = historydata[i][0] + SECONDS_FROM_1970_TO_2000;
+    historydata[i][0] = new Date(unixtime * 1000);
+  }
+  document.write("finish<br>");
+
+  //Google Stuff
+  google.charts.load('current', {packages: ['corechart']});
+  google.charts.setOnLoadCallback(loadData);
+
+  function loadData() {
+    var dt_history = new google.visualization.DataTable({
+      cols: [{id: 'timestampUTC', label: 'Task', type: 'date'},
+             {id: 'hx_hot_inlet_ave', label: 'Temperature from panels', type: 'number'},
+             {id: 'hx_hot_outlet_ave', label: 'Temperature to panels', type: 'number'},
+             {id: 'hx_cold_inlet_ave', label: 'Temperature from pool', type: 'number'},
+             {id: 'hx_cold_outlet_ave', label: 'Temperature to pool', type: 'number'},
+             {id: 'ambient_temperature', label: 'Ambient temperature', type: 'number'},
+             {id: 'cumulative_insolation', label: 'Sunshine since midnight', type: 'number'},
+            ]
+    	});
+    dt_history.addRows(historydata);	 
+    g = new Dygraph(
+        document.getElementById("graphdiv"),  // containing div
+        dt_history,
+        { }                                   // the options
+      );
+  }
+</script>
+</body>
+</html>
--- php/get_historical.php
+++ php/get_historical.php
@@ -2,7 +2,7 @@
     require "db_historical_connect.php";
 
     // Get the latest sensor reading, round the result for each sensor and update webpage
-    $sql = "SELECT `timestampUTC`, `hx_hot_inlet_ave`, `hx_hot_outlet_ave`, `hx_cold_inlet_ave`,  `hx_cold_outlet_ave`,  `temp_ambient_ave`,  `cumulative_insolation` FROM `LoggedData` ORDER BY timestampUTC LIMIT 2000;";
+    $sql = "SELECT `timestampUTC`, `hx_hot_inlet_ave`, `hx_hot_outlet_ave`, `hx_cold_inlet_ave`,  `hx_cold_outlet_ave`,  `temp_ambient_ave`,  `cumulative_insolation` FROM `LoggedData` ORDER BY timestampUTC LIMIT 20000;";
     $result = mysqli_query($conn_hist, $sql);
     $historydata = mysqli_fetch_all($result);
     mysqli_free_result($result);
@@ -14,58 +14,3 @@ if(empty($historydata)) {
 }
 ?>
 
-<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-<script type="text/javascript">
-    // use json_encode to pass data from PHP to javascript literal
-    var historydata = <?php echo json_encode($historydata); ?>;
-
-    var SECONDS_FROM_1970_TO_2000 = 946684800;
-    var unixtime = 0;	
-   document.write("start<br>");
-    // convert timestamp unixtime to javascript dates
-   hdlen = historydata.length;	
-   for (i = 0; i < hdlen; i++) {
-//     document.write(historydata[i][0]);
-//     document.write(" = ");
-     unixtime = parseInt(historydata[i][0]) + SECONDS_FROM_1970_TO_2000;
-     historydata[i][0] = new Date(unixtime* 1000);
-//     document.writeln(historydata[i][0]);
-//     document.write("<br>");
-   }
-   document.write("finish<br>");
-
-//   for (i = 0; i < hdlen; i++) {
-//     hdcol = historydata[i].length;
-//     for (j = 0; j < hdcol; ++j) {
-//       document.write(historydata[i][j]);
-//     }  
-//     document.write("<br>");
-//   }
-
-    //Google Stuff
-//    google.charts.load('current', {packages: ['corechart']});
-//    google.charts.setOnLoadCallback(drawChart);
-
-//    function drawChart() {
-        // Define the chart to be drawn.
-//        var data = new google.visualization.DataTable();
-//        data.addColumn('string', 'Element');
-//        data.addColumn('number', 'Percentage');
-//        data.addRows(rows);         
-
-        // Set chart options
-  //      var options = {
-//            title:'Sales',
-//            titleTextStyle: {color: 'white', bold: true},
-//            height:300,
-//            backgroundColor: '#3a3939',
-//            color: '#fff',
-//            legend: {textStyle: {color: 'white'}},
-//            hAxis: {gridlines: {color: 'black'}}
-//        };
-
-//        // Instantiate and draw the chart.
-//        var chart = new google.visualization.PieChart(document.getElementById('SalesChar'));
-//        chart.draw(data, options );
-//    }
-</script>
